3debaf93cf8466df832958ef868b6be7f08de5a6
Victor Lyapunov <viclyapunov@mail.ru>
2017-09-16 03:19:03 +0000

    Commit before trying out Phinx

--- public/index.php
+++ public/index.php
@@ -7,10 +7,21 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
 
-    return $response;
+$container = $app->getContainer();
+
+$container['view'] = function($container) {
+	$templates = __DIR__ . '/../templates/';
+
+	$view = new Slim\Views\Twig($templates, compact('cache'));
+
+	return $view;
+};
+
+$app->get('/', function (Request $request, Response $response, $args) {
+	return $this->view->render($response, 'home.twig', [
+		'name' => $request->getParam('name'),
+	]);
 });
+
 $app->run();
